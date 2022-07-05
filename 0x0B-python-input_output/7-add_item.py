7f2288912bc2c56bf5fcfbb38aabc1d156802e8c
Camilacamelyn <camillahcamelyn1@gmail.com>
2022-07-05 07:36:59 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-script that adds all arguments to a python list,
-and saves them to a file.
+Script that adds all arguments to a Python list,
+and then save them to a file.
 """
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-try
-my_list = load_from_json_file("add_item.json")
+try:
+    arg_list = load_from_json_file("add_item.json")
 except Exception:
-    my_list = []
+    arg_list = []
 
-    for element in range(1, len(sys.argv)):
-        my_list.append(args[1:])
-    save_to_json_file(my_list, "add_item.json")
+for element in range(1, len(sys.argv)):
+    arg_list.append(sys.argv[element])
+save_to_json_file(arg_list, "add_item.json")
